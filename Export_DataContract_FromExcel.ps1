c2e765dbe422a86ba573f0b91f55fcb31008868c
Aaron Nelson <aaron.nelson@microsoft.com>
2022-08-01 07:59:49 -0400

    Re-tested.

--- Export_DataContract_FromExcel.ps1
+++ Export_DataContract_FromExcel.ps1
@@ -1,2 +1,2 @@
 $DataContract = Import-Excel .\DataContract_AdventureWorksSQL.XLSX * | ConvertTo-Json -Depth 3
-$DataContract | Out-File DataContract_AdventureWorksSQL.JSON
+$DataContract | Out-File .\DataContract_AdventureWorksSQL.json
